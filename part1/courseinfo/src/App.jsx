8e41014a92589528b00790e2a87d72ea639dfbcb
haizeagc <116098300+haizeagc@users.noreply.github.com>
2024-10-29 10:51:25 +0100

    React changes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,4 @@
+
 const Header =(props) => {
   return (
     <h1>{props.course}</h1>
@@ -6,7 +7,17 @@ const Header =(props) => {
 
 const Content = (props) => {
   return (
-    
+    <div>
+    <Part part={props.part} exercises={props.exercises}/>
+    <Part part2={props.part} exercises2={props.exercises}/>
+    <Part part3={props.part} exercises3={props.exercises}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
@@ -28,17 +39,11 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      
+      <Content part={part1} exercises={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+
+      <Total exercises1={exercises1}  exercises2={exercises2}  exercises3={exercises3}/>
     </div>
   )
 }
