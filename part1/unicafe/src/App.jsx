fb8319d675ed64db852441025d5e1f19379499d4
haizeagc <116098300+haizeagc@users.noreply.github.com>
2024-11-26 12:03:27 +0100

    Lo del props

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
 
-/*
+
 const Statistics = (props) => {
   return (
   <div>
 
   <h1>Statistics</h1>
 
-  <p>Good</p>
-  <p>Neutral</p>
-  <p>Bad</p>
-  <p>All</p>
-  <p>Average </p>
-  <p>Positive </p>
+  if(props.good + props.bad + props.neutral === 0){
+    <p>No feedback </p>
+  } else {
 
+    <p>Good {props.good}</p>
+    <p>Neutral {props.neutral}</p>
+    <p>Bad {props.bad}</p>
+    <p>All {props.good + props.neutral + props.bad}</p>
+    <p>Average {(props.good - props.bad)/(props.good + props.neutral + props.bad)}</p>
+    <p>Positive {props.good/(props.good + props.neutral + props.bad)*100}%</p>
+  }
   </div>
   )
 }
-*/
+
 
 const App = () => {
 
@@ -40,21 +44,14 @@ const App = () => {
     <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
     <button onClick={() => setBad(bad + 1)}>bad</button>
 
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>average {(good - bad)/(good + neutral + bad)}</p>
-    <p>positive {good/(good + neutral + bad)*100}%</p>
+     {/*No funcionan los numeros el texto si
+     Tiene pinta que no va el setStatus bien con el componente */}
+
+    <Statistics good={good.good} neutral={neutral.neutral} bad={bad.bad}/>  
+
    
     </div>
   )
 }
-
-/*
-
-    <Statistics good={good.good} neutral={neutral.neutral} bad={bad.bad}/>  
-
-*/
 
 export default App
